4033f9b07bb00b819308b4d013ce61b7d14d1b3e
Mik-pvq <mikss22main@gmail.com>
2024-10-25 15:46:55 +0200

    even_odd solution

---
+++ 1/even-odd/even_odd_sol.ml
@@ -0,0 +1,8 @@
+let is_even n = (n mod 2 = 0);;
+
+let check num = (num>0 && num<6)
+
+let win a b = if not(check a) && not(check b) then 0
+else if (not(check a) || not(check b)) then 
+  (if (check a) then 1 else -1)
+else if (is_even a) then 1 else -1;;
