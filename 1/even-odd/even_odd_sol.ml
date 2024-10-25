085aa110edb4b84e85704ce7c6d12c8a62f88941
Mik-pvq <mikss22main@gmail.com>
2024-10-25 15:52:41 +0200

    even_odd solution correct

--- 1/even-odd/even_odd_sol.ml
+++ 1/even-odd/even_odd_sol.ml
@@ -5,4 +5,4 @@ let check num = (num>0 && num<6)
 let win a b = if not(check a) && not(check b) then 0
 else if (not(check a) || not(check b)) then 
   (if (check a) then 1 else -1)
-else if (is_even a) then 1 else -1;;
+else if (is_even (a + b)) then 1 else -1;;
