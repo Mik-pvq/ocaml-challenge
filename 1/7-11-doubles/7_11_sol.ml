52cf41b682be32ba108e7659d31466ff23f2c00a
Mik-pvq <mikss22main@gmail.com>
2025-10-10 12:39:07 +0200

    corrected 7-11

--- 1/7-11-doubles/7_11_sol.ml
+++ 1/7-11-doubles/7_11_sol.ml
@@ -1,4 +1,6 @@
-let seven_eleven_doubles d1 d2 = 
-  if ((d1+d2) = 7 || (d1+d2) = 11 || d1=d2) then (true, d1, d2)
-  else (false, d1, d2);;
+let seven_eleven_doubles () = 
+  let d1 = Random.int(6)+1 in
+  let d2 = Random.int(6)+1 in
+  (d1=d2 || d1+d2=7 || d1+d2=11, d1, d2)
+;;
     
