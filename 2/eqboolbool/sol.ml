03fcb24303bbee86e3a0d9d8f342ec524ca1e9be
Mik-pvq <mikss22main@gmail.com>
2025-10-13 22:51:44 +0200

    solved eqboolbool

---
+++ 2/eqboolbool/sol.ml
@@ -0,0 +1,9 @@
+let ( =? ) f g = let inutile = f true || g true in
+  (f true = g true) && (f false = g false)
+;;
+
+assert ((fun b -> true) =? (fun b -> true));;
+assert ((fun b -> b) =? (fun b -> true) = false);;
+assert ((fun b -> false) =? (fun b -> true) = false);;
+assert (not =? (fun b -> not (not b)) = false);;
+assert ((fun b -> b) =? (fun b -> not (not b)));;
