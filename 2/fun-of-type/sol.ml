c9b3dde25d3e2053574aa9eeea196b84eec45103
Mik-pvq <mikss22main@gmail.com>
2025-10-13 23:01:00 +0200

    init fun of type

---
+++ 2/fun-of-type/sol.ml
@@ -0,0 +1,39 @@
+let f1 n = n>4;;
+
+let f2 p = if p then 3 else 4;;
+
+let f3 n = (n, n>3);;
+
+let f4 (n, v) = if v then n else 0;;
+
+let f5 a b= a+b ;;
+
+let f6 a b = a = 2 && b = 4;;
+
+let f7 p n = if p then 4 else if n = 4 then 5 else 0;;
+
+let f8 p q = if p then 0 else if q then 2 else 6;;
+
+let f9 = ;;
+
+let f10 = ;;
+
+let f11 = ;;
+
+let f12 = ;;
+
+let f13 = ;;
+
+let f14 = ;;
+
+let f15 = ;;
+
+let f16 = ;;
+
+let f17 = ;;
+
+let f18 = ;;
+
+let f19 = ;;
+
+let f20 = ;;
